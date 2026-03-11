0e5fd6619cb0fa1598a913ce8e1ed7079ff98ef1
Sivakumar-streamforce <167304919+SivaStreamForce@users.noreply.github.com>
2026-03-12 02:12:13 +0530

    fix github auth for gitops push

--- update-image.sh
+++ update-image.sh
@@ -2,7 +2,7 @@
 
 TAG=$1
 
-sed -i "s|image:.*|image: yourdockerhub/devops-flask-app:$TAG|g" deployment.yaml
+sed -i "s|image:.*|image: sivakumar123/devops-flask-app:$TAG|g" deployment.yaml
 
 git config --global user.email "ci@example.com"
 git config --global user.name "github-actions"
